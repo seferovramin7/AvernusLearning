ad04f2be2b0fd7eb62c4b0ef49837a0dc4fdc071
seferovramin7 <seferovramin7@gmail.com>
2025-02-03 20:26:53 +0400

    update

--- src/IlkDers.java
+++ src/IlkDers.java
@@ -1,7 +1,14 @@
 public class IlkDers {
-  //  psvm
-  //  sout
-  public static void main(String[] args) {
-      System.out.println(2 + 2);
-  }
+    //  psvm
+    //  sout
+    public static void main(String[] args) {
+
+        double menimPi = 3.1415;
+        int teref = 3;
+        String menimCumle = "Kvadratin perimetri : ";
+
+        System.out.println(menimCumle + (teref * 4));
+
+
+    }
 }
